66629b9cc4046d7b872e184770974350c939b880
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2023-11-07 11:02:00 +0000

    Update copyConfig.js

--- scripts/Outsystems/copyConfig.js
+++ scripts/Outsystems/copyConfig.js
@@ -18,7 +18,7 @@ module.exports = function (context) {
         path  = context.requireCordovaModule("path");
     }
 
-    //mlrosa - Identify AppId, need this code to place the dynatrace config in the Environment folder. We have 4 Environments
+    //Miguel Rosa - Identify AppId, need this code to place the dynatrace config in the Environment folder. We have 4 Environments
     function getAppId(context) {
       var cordovaAbove8 = isCordovaAbove(context, 8);
       var et;
